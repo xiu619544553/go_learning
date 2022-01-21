f9a89ed557c7e078a7c67becbf580466e9848aa3
hanxiuhui <hanxiuhui@offcn.com>
2022-01-21 17:20:37 +0800

    <feature>error

--- main.go
+++ main.go
@@ -34,11 +34,14 @@ func main () {  // 声明 main 主函数
 	// fmt.Println("==========闭包==========")
 	// aboutClosure()
 
-	fmt.Println("========== defer 延迟调用 ==========")
-	aboutDefer()
+	// fmt.Println("========== defer 延迟调用 ==========")
+	// aboutDefer()
 
 	// fmt.Println("========== 闭包，Defer，参数传递，变量作用域 ==========")
 	// aboutDeferClosure()
+
+	fmt.Println("========== 异常处理 ==========")
+	aboutError()
 }
 
 
